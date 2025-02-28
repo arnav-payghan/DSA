c69b40f8e90ad451a1553181d0b2ab0320ab6d38
Arnav Payghan <arn.payghan@gmail.com>
2025-03-01 05:27:40 +0530

    Issues are still there, 'extract' and java files are not working, may need to look into it.

---
+++ introductionToJava/inputOutput/InputOutput.java
@@ -0,0 +1,14 @@
+package introductionToJava.inputOutput;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        // Learning of basics --> Introduction, Output, Operators, Data Types.
+
+        // Rule1: Terminate line with a semicolon;
+        // Rule2: Java is a case-sensitive language.
+        // Rule3: Inside the paranthesis, you can use mathematics.
+        // Rule4: Use double quotes for String o/p.
+               
+        System.out.println(10);   // Output
+    }
+}
--- introductionToJava/test.java
+++
@@ -1,7 +0,0 @@
-package introductionToJava;
-
-class test {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
