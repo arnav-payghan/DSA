dae2690bc4a4275877da6b3231bb690af8ef1528
Arnav Payghan <arn.payghan@gmail.com>
2025-03-02 21:57:52 +0530

    Added remaining Data Types.

--- introductionToJava/inputOutput/InputOutput.java
+++ introductionToJava/inputOutput/InputOutput.java
@@ -10,7 +10,14 @@ public class InputOutput {
         // Rule4: Use double quotes for String o/p.
         // Rule5: You can concatenate Sys.print.out(1+1+"Hello"); === 2Hello.
         // Rule6: Operators follow BODMAS/BIDMAS.
+        // Rule7: You can't create 2 or more variables with the same name.
         
-        System.out.println(4+3*6-7/2);   // Output
+        System.out.println(4+3*6-7/2);   // Output = 19
+
+        // DATA TYPES : Numbers(Integer, Decimal-->Float(7 decimal digits)), Character(a,b,c), Double(16 decimal digits),
+        //              Boolean(T/F), etc.
+
+        // int, float, char, boolean, float, double,
+
     }
 }
