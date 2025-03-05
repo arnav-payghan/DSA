66bdf68bd52e59a6cad577c605a805455f130cc4
Arnav Payghan <arn.payghan@gmail.com>
2025-03-05 23:09:25 +0530

    Added Syntax and Voter Availability.

--- introductionToJava/dataTypeAndOperators/DataTypeVariables.java
+++ introductionToJava/dataTypeAndOperators/DataTypeVariables.java
@@ -15,6 +15,17 @@ public class DataTypeVariables {
         System.out.println(y);  // Output: 1000000000000000000
         int x = (int)y;
         System.out.println(x);  // Output: -1486618624 Because out of range for int conversion.
+        
+        double d = 10.00;
+        System.out.println(d);
+
+        float f = 16.72f;
+        System.out.println(f);
+
+        // OPERATIONS :
+        //  Rule1: Mathematical operation between decimal and non-decimal operations results in decimal answers.
+        //  Rule2: Operation b/w same category but different capacity. RESULTANT: Higher Capacity.
+        //         Int + Int == Int, Int + Double == Double, and so on...
 
     }
 }
--- introductionToJava/ifElse/IfElse.java
+++ introductionToJava/ifElse/IfElse.java
@@ -2,6 +2,20 @@ package introductionToJava.ifElse;
 
 public class IfElse {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // If-Else statements.
+
+        // if ( condition ) {
+        //     block of code
+        // } else {
+        //     block of code
+        // }
+
+        int age = 25;   // Change to keep checking the answers.
+
+        if ( age >= 18 ) {
+            System.out.println("You are eligible to Vote!");
+        } else {
+            System.out.println("You are NOT eligible to Vote!");
+        }
     }
 }
