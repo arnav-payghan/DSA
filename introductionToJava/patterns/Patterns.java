7159dfafe4646d58047dcbdad8361634f7163a98
Arnav Payghan <arn.payghan@gmail.com>
2025-03-14 02:04:39 +0530

    Patterns folder/file committed.

--- introductionToJava/patterns/Patterns.java
+++ introductionToJava/patterns/Patterns.java
@@ -3,5 +3,6 @@ package introductionToJava.patterns;
 public class Patterns {
     public static void main(String[] args) {
         // Pattern making in Java.
+        System.out.println("Hello, Patterns!");
     }
 }
