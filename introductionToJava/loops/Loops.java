aa1e1f94d33c4abcca1d3b263897a539dc80a8dc
Arnav Payghan <arn.payghan@gmail.com>
2025-03-13 16:45:35 +0530

    Added Loops Part 1 File.

---
+++ introductionToJava/loops/Loops.java
@@ -0,0 +1,13 @@
+package introductionToJava.loops;
+
+public class Loops {
+    public static void main(String[] args) {
+        int count = 1;
+        for (int i=0; i<10; i++) {
+            System.out.println(count);
+            count++;
+        }
+
+        
+    }
+}
