32732ba1f13e88d8d443ecc6e142ab10a07c73fa
Arnav Payghan <arn.payghan@gmail.com>
2025-03-14 02:29:59 +0530

    Solved 2 Pattern Questions.

--- introductionToJava/patterns/Patterns.java
+++ introductionToJava/patterns/Patterns.java
@@ -3,6 +3,21 @@ package introductionToJava.patterns;
 public class Patterns {
     public static void main(String[] args) {
         // Pattern making in Java.
-        System.out.println("Hello, Patterns!");
+
+        for (int i = 0; i<5; i++) {
+            for (int j = 0; j<=i; j++) {
+                System.out.print("x ");
+            }
+            System.out.println("");
+        }
+
+        // For given integer N, draw it's square, take N as 3 and 5.
+
+        for (int N=0; N<5; N++) {
+            for (int C=0; C<5; C++) {
+                System.out.print("x ");
+            }
+            System.out.println("");
+        }
     }
 }
