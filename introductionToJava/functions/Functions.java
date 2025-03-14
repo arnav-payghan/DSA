a07e83084083770181ef62c716dc621596176441
Arnav Payghan <arn.payghan@gmail.com>
2025-03-14 11:46:39 +0530

    Added Functions Folder/File.

---
+++ introductionToJava/functions/Functions.java
@@ -0,0 +1,8 @@
+package introductionToJava.functions;
+
+public class Functions {
+    public static void main(String[] args) {
+        // We'll be learning about Functions.
+        System.out.println("Hello, Functions!");
+    }
+}
